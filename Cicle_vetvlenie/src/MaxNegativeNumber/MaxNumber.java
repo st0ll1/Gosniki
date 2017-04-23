0473fca2ba8ff8065d75bd8848d5d08d4c6f3e1d
st0ll1 <stolli@list.ru>
2017-04-23 13:20:27 +0300

    Add MaxNumber change

--- Cicle_vetvlenie/src/MaxNegativeNumber/MaxNumber.java
+++ Cicle_vetvlenie/src/MaxNegativeNumber/MaxNumber.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class MaxNumber {
     public static void main(String[] args) {
 
-        int minNumber = Integer.MIN_VALUE;             // минимальное число из отрицательных
+        //int minNumber = Integer.MIN_VALUE;             // минимальное число из отрицательных
         int number = 0;                                // число в последовательности
         int quantity = 0;                              // длина последовательности
 
@@ -32,7 +32,7 @@ public class MaxNumber {
         for (int i = 0; i < quantity; i++) {
             System.out.print("Введите число: ");
             number = input.nextInt();
-            if (number < 0 && number > minNumber) {
+            if (number < 0 ) {                          // && number > minNumber
                 flag = true;
                 arrayInt[i] = number;
             }
@@ -46,12 +46,12 @@ public class MaxNumber {
 /* -------------------------------------- Поиск максимального отрицательного числа --------------------------------- */
 
         int maxNumber = arrayInt[0];
-        for (int i = 0; i < arrayInt.length; i++) {
+        for (int i = 1; i < arrayInt.length; i++) {
             if(arrayInt[i] == 0) {
                 continue;
-            } else if (arrayInt[i] >= minNumber)
+            } else if (arrayInt[i] >= maxNumber)
                 maxNumber = arrayInt[i];
-                minNumber = arrayInt[i];
+                //minNumber = arrayInt[i];
         }
         System.out.println("Максимальное неположительное число в последовательности: "
                 + maxNumber);
