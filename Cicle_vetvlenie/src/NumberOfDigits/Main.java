b2414240fa4ad9f3401f90adcb40369c39faf4ee
st0ll1 <stolli@list.ru>
2017-04-23 12:33:09 +0300

    Add NumberOfDigits change

--- Cicle_vetvlenie/src/NumberOfDigits/Main.java
+++ Cicle_vetvlenie/src/NumberOfDigits/Main.java
@@ -2,14 +2,17 @@ package NumberOfDigits;
 
 import javax.swing.JOptionPane;
 
+/*
+ * Задача нахождения количества цифр в числе N (N < 10^9)
+ */
 
 public class Main {
     public static void main(String[] args) {
 
-        int number = 1;             // количество цифр
+        int number = 1;                              // количество цифр
         Integer digit = 0;
         try {
-            // Число в котором считаем количество цифр
+            // Вводим число в котором считаем количество цифр
              digit = Integer.parseInt(JOptionPane.showInputDialog("Введите число"));
 
         } catch (NumberFormatException ex) {
@@ -18,6 +21,7 @@ public class Main {
             System.exit(0);
         }
 
+        // Здесь сам алгоритм
         while ((digit /= 10) > 0) {
             if(digit>(Math.pow(10, 9))) {
                 JOptionPane.showMessageDialog(null,"Число больше максимально допустимого",
@@ -26,7 +30,7 @@ public class Main {
             }
             number++;
         }
-
+        // Выводим в графическое окно результат
         JOptionPane.showMessageDialog(null,"Количество цифр в числе: "+number,
                 "Количество цифр в числе", JOptionPane.PLAIN_MESSAGE);
     }
