86357cb01e067d677f46cda6dcd9860deb9dbfe6
st0ll1 <stolli@list.ru>
2017-04-23 18:42:27 +0300

    Add task OperationsOnNumber

---
+++ Cicle_vetvlenie/src/OperationsOnNumbers/NumberOperation.java
@@ -0,0 +1,55 @@
+package OperationsOnNumbers;
+
+import javax.swing.*;
+
+/**
+ * st0ll1
+ * 23.04.17
+ */
+public class NumberOperation {
+    public static void main(String[] args) {
+
+        // строка с операцией
+        String str;
+
+        str = JOptionPane.showInputDialog(null, "Введите выражение",
+                "Ввод операции", JOptionPane.PLAIN_MESSAGE);
+
+        String [] strOperation = str.split(" ");
+
+        Double firsOperand = Double.parseDouble(strOperation[0]);
+        Double lastOperand = Double.parseDouble(strOperation[2]);
+
+        // Форматированная строка вывода
+        String outString = String.format("%s %s %s = %.2f", firsOperand, strOperation[1],lastOperand,
+                (firsOperand+lastOperand));
+
+        switch (strOperation[1]) {
+            case "+" :
+                JOptionPane.showMessageDialog(null,outString, "Результа вычислений",
+                        JOptionPane.PLAIN_MESSAGE);
+                break;
+
+            case "-" :
+                JOptionPane.showMessageDialog(null,outString, "Результа вычислений",
+                        JOptionPane.PLAIN_MESSAGE);
+                break;
+
+            case "/" :
+                JOptionPane.showMessageDialog(null,outString, "Результа вычислений",
+                        JOptionPane.PLAIN_MESSAGE);
+                break;
+
+            case "*" :
+                JOptionPane.showMessageDialog(null,outString, "Результа вычислений",
+                        JOptionPane.PLAIN_MESSAGE);
+                break;
+
+            default:
+                JOptionPane.showMessageDialog(null,"Неизвестная операция!!!", "Error!!!",
+                        JOptionPane.ERROR_MESSAGE);
+                break;
+
+        }
+    }
+}
