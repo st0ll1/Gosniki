4a1eb1a3d409e3b1dae3218ebeacf4ef18e76aec
st0ll1 <st0ll1@yandex.ru>
2017-06-05 10:20:39 +0300

    Update MaxNumber.java

--- Cicle_vetvlenie/src/Task1_Branches_Cicles/MaxNegativeNumber/MaxNumber.java
+++ Cicle_vetvlenie/src/Task1_Branches_Cicles/MaxNegativeNumber/MaxNumber.java
@@ -1,5 +1,6 @@
 package Task1_Branches_Cicles.MaxNegativeNumber;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -10,7 +11,6 @@ import java.util.Scanner;
 public class MaxNumber {
     public static void main(String[] args) {
 
-        //int minNumber = Integer.MIN_VALUE;             // минимальное число из отрицательных
         int number = 0;                                // число в последовательности
         int quantity = 0;                              // длина последовательности
 
@@ -27,14 +27,17 @@ public class MaxNumber {
 
 /* -------------------------------------- Ввод элементов последовательности ---------------------------------------- */
         boolean flag = false;                          // флаг наличия отрицательного числа в последовательности
-        int[] arrayInt = new int[quantity];            // добавим массив для хранения отрицательных чисел
-
+        
+        // добавим массив для хранения отрицательных чисел
+        ArrayList<Integer> arrayInteger = new ArrayList<Integer>(quantity);
+        
         for (int i = 0; i < quantity; i++) {
             System.out.print("Введите число: ");
             number = input.nextInt();
             if (number < 0) {                          // && number > minNumber
+                arrayInteger.ensureCapacity(i+1);
                 flag = true;
-                arrayInt[i] = number;
+                arrayInteger.add(number);
             }
         }
 
@@ -45,12 +48,12 @@ public class MaxNumber {
 
 /* -------------------------------------- Поиск максимального отрицательного числа --------------------------------- */
 
-        int maxNumber = arrayInt[0];
-        for (int i = 0; i < arrayInt.length; i++) {
-            if (arrayInt[i] == 0) {
+        int maxNumber = arrayInteger.get(0);
+        for (int i = 0; i < arrayInteger.size(); i++) {
+            if (arrayInteger.get(i) >= 0) {
                 continue;
-            } else if (arrayInt[i] >= maxNumber)
-                maxNumber = arrayInt[i];
+            } else if (arrayInteger.get(i) >= maxNumber)
+                maxNumber = arrayInteger.get(i);
             //minNumber = arrayInt[i];
         }
         System.out.println("Максимальное неположительное число в последовательности: "
