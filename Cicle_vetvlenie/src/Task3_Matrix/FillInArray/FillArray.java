a99204d26341518e01e0e2a44c0d980b7570b6a9
st0ll1 <st0ll1@yandex.ru>
2017-06-09 13:37:30 +0300

    Create FillArray.java

--- Cicle_vetvlenie/src/Task3_Matrix/FillInArray/FillArray.java
+++ Cicle_vetvlenie/src/Task3_Matrix/FillInArray/FillArray.java
@@ -7,7 +7,26 @@ package Task3_Matrix.FillInArray;
  */
 public class FillArray {
     public static void main(String[] args) {
-
+          int N = 10;
+ -        int M = 10;
+ -        int number = 1;
+ -        int[][] twoMatrix = new int[N][M];
+  
+ -        // заполним матрицу заданным образом
+ -        for (int i = 0; i < N; i++) {
+ -            for (int j = 0; j < M; j++) {
+ -                twoMatrix[i][j] = number++;
+ -            }
+ -            number = number - (N - 1);
+ -        }
+  
+ -        // выведем матрицу
+ -        for (int i = 0; i < N; i++) {
+ -            System.out.println();
+ -            for (int j = 0; j < M; j++) {
+ -                System.out.print(twoMatrix[i][j] + " ");
+ -            }
+ -        }
 
 
     }
